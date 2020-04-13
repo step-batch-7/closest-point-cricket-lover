302e64a0b53d3daabe4db0d1b278a3d447cf1246
phaneendra <phaneendraandukuri@gmail.com>
2020-04-13 17:47:17 +0530

    made a typedef for struct Point as Point

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	struct Point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}}, current_location = {10, 94}, *closest_food_location;
+	Point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}}, current_location = {10, 94}, *closest_food_location;
 	get_closest_food(food_points, 5, current_location, closest_food_location);
 	printf("closest food location is [%d, %d]\n", closest_food_location->x, closest_food_location->y);
 	return 0;
--- point.c
+++ point.c
@@ -1,14 +1,14 @@
 #include <math.h>
 #include "point.h"
 
-double get_distance(struct Point start, struct Point end)
+double get_distance(Point start, Point end)
 {
 	double sqr_of_delta_in_x = pow(end.x - start.x, 2);
 	double sqr_of_delta_in_y = pow(end.y - start.y, 2);
 	return sqrt(sqr_of_delta_in_x + sqr_of_delta_in_y);
 }
 
-void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location)
+void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location)
 {
 	double previous_distance = get_distance(current_location, food_points[0]);
 	*closest_food_location = food_points[0];
