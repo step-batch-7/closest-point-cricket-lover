2abbe831a00635221a8b9fa90ca786e287e446b7
phaneendra <phaneendraandukuri@gmail.com>
2020-04-13 18:03:12 +0530

    cleaned up code and changed type of closest_food_location

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 
 int main(void)
 {
-	Point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}}, current_location = {10, 94}, *closest_food_location;
-	get_closest_food(food_points, 5, current_location, closest_food_location);
-	printf("closest food location is [%d, %d]\n", closest_food_location->x, closest_food_location->y);
+	Point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
+	Point current_location = {10, 94};
+	Point closest_food_location = {0,0};
+	
+	get_closest_food(food_points, 5, current_location, &closest_food_location);
+	printf("closest food location is [%d, %d]\n", closest_food_location.x, closest_food_location.y);
 	return 0;
 }
